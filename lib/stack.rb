081d283a287f1cac5c144fa6158a3024a74585a4
Mike Starr <imhs@protonmail.com>
2022-03-22 21:19:08 -0700

    commit tree aa7a620e96f294f3236efdb73769aec64efe66a6

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,23 @@
-# your code here
+require 'pry'
+class Stack
+    @stack = []
+
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+        @stack.push value
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.peek
+    end
+    binding.pry
+end
+
+Stack.new
